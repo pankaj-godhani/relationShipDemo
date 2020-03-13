1d27d644dacb7dd93ae7d88e1a4ecfb4c80b7bf1
pankaj-godhani <godhanipnj131@gmail.com>
2020-03-13 19:51:59 +0530

    first commit

---
+++ app/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = 'user';
+    public function supplier()
+    {
+        return $this->hasMany('App\Supplier');
+    }
+}
