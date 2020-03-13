97bc79fbd18b5a3da659a4c7a5533240b1780238
pankaj-godhani <godhanipnj131@gmail.com>
2020-03-13 19:56:46 +0530

    space in user model.

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $table = 'user';
+
     public function supplier()
     {
         return $this->hasMany('App\Supplier');
